4f3cfd1ea70e46b6a66ded91cfefa5ebbe5cbf8e
nithishvarmurugesh04-hub <nithishvarmurugesh04@gmail.com>
2026-09-24 14:13:19 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from routes import reports, gis
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from datetime import datetime, timezone
